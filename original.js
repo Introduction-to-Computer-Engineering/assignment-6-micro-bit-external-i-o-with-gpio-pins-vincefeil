e2b42aa248f341c0016d99fc5e2cf5365630c01d
vincefeil <54681226+vincefeil@users.noreply.github.com>
2019-10-23 09:23:52 -0600

    add comments

--- original.js
+++ original.js
@@ -1,11 +1,13 @@
-led.enable(false)
-pins.analogWritePin(AnalogPin.P6, 0)
+//Change the brightness of the three LEDs from off to full on
+led.enable(false) //Turn off onboard LED display
+pins.analogWritePin(AnalogPin.P6, 0) //Set digital pins to 0
 pins.analogWritePin(AnalogPin.P8, 0)
 pins.analogWritePin(AnalogPin.P9, 0)
 basic.forever(function () {
+    //for the index variable set a rage between 0 and 3071 start at 0 and add 1 through the range
     for (let index = 0; index <= 3071; index++) {
         if (index < 2046) {
-            if (index <= 1023) {
+            if (index <= 1023) {//if the index value is <
                 pins.analogWritePin(AnalogPin.P6, index)
             } else {
                 pins.analogWritePin(AnalogPin.P6, 1023 - (index - 1023))
